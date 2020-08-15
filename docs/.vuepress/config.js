938bdceb2b59487ef465945ed285181f3a3fa503
叶恒 <yeheng@zuzuche.com>
2020-08-16 00:04:39 +0800

    添加sidebar

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'title': '😤😤',
+    'description': '',
+    'base': '/',
+    themeConfig: {
+        sidebar: [
+            '/',
+            '/Homebrew使用技巧'
+        ]
+    }
+}
